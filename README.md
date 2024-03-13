7cce3c6eed74714ae64785adf49b2eb8b6e1be2a
Joe Corall <jjc223@lehigh.edu>
2024-03-13 16:23:18 -0400

    Initial port to Drupal 10

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 ## Introduction
 
-Islandora Rightsstatements provides block containing a Rightsstatements.org button with a link to the statement URI and its details.
-It reads the Rightsstatements.org URI from a Solr field, and builds the HTML from the appropriate assets at Rightsstatements.org.
+Islandora Rightsstatements provides a field formatter containing a Rightsstatements.org button with a link to the statement URI and its details.
+
+It reads the Rightsstatements.org URI from a Drupal field, and builds the HTML from the appropriate assets at Rightsstatements.org.
 
 Example badge image:
 
@@ -11,52 +12,24 @@ Example badge image:
 
 The badge will only display on objects with a Rightsstatements.org URI in some defined element.
 
-## Requirements
-
-This module requires the following modules/libraries:
-
-* [Islandora](https://github.com/islandora/islandora)
-* [Islandora Solr](https://github.com/islandora/islandora_solr)
-* [Islandora Badges](../../)
-
 ## Installation
 
-Install as usual, see [this](https://drupal.org/documentation/install/modules-themes/modules-7) for further information.
+Install as usual, see [https://www.drupal.org/docs/extending-drupal/installing-modules](https://www.drupal.org/docs/extending-drupal/installing-modules) for further information.
 
 ## Configuration
 
-Configuration path is admin/islandora/tools/badges/rightsstatements (Administration > Islandora > Islandora Utility Modules > Islandora Badges Configuration > Rightsstatements Badge).
-
-* Solr field for Rightsstatements.org URI: Defaults to mods_accessCondition_ms. dc.rights would also be a good choice. Prefer a multivalued field.
-* Image style: Choose small icon (just the symbol) or Large button (symbol plus text).
-* Image Height: Set the image height in pixels.
-* Image Colour: Rightsstatements.org provides black or white icons, and buttons with black text or blue text.
-
-## Metadata
-
-You can choose any element for your Rights Statement URI, so long as it creates a Solr field. The default is `dc.rights`.
-
-The value of the field must be a URI to the Rights Statement. Example from a MODS datastream:
-
-`<accessCondition type="use and reproduction" displayLabel="Rights Statement">http://rightsstatements.org/vocab/InC-NC/1.0/</accessCondition>`
-
-## Troubleshooting/Issues
-
-Having problems or solved a problem? Check out the Islandora google groups for a solution.
-
-* [Islandora Group](https://groups.google.com/forum/?hl=en&fromgroups#!forum/islandora)
-* [Islandora Dev Group](https://groups.google.com/forum/?hl=en&fromgroups#!forum/islandora-dev)
+Use the Rights Statement badge field formatter on your `field_rights`
 
 ## Maintainers/Sponsors
 
 Current maintainers:
 
-* [Brandon Weigel](https://github.com/bondjimbond)
-
-## Development
-
-If you would like to contribute to this module, please check out [CONTRIBUTING.md](CONTRIBUTING.md). In addition, we have helpful [Documentation for Developers](https://github.com/Islandora/islandora/wiki#wiki-documentation-for-developers) info, as well as our [Developers](http://islandora.ca/developers) section on the [Islandora.ca](http://islandora.ca) site.
+* [Joe Corall](https://github.com/joecorall)
 
 ## License
 
 [GPLv3](http://www.gnu.org/licenses/gpl-3.0.txt)
+
+## Attribution
+
+This module began as a fork/port of the Drupal 7 module [bondjimbond/islandora_rightsstatements](https://github.com/bondjimbond/islandora_rightsstatements)
---
+++ src/Plugin/Field/FieldFormatter/RightsStatementBadge.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace Drupal\islandora_rights_statements\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Plugin implementation of the 'islandora_rights_statements' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "islandora_rights_statements",
+ *   label = @Translation("Islandora Rights Statement Badge"),
+ *   field_types = {
+ *     "string",
+ *     "text"
+ *   }
+ * )
+ */
+class RightsStatementBadge extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'button_style' => 'buttons',
+      'image_height' => '31',
+      'image_color' => 'dark',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['button_style'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Image style'),
+      '#description' => $this->t('The type of image returned as your Rights Statements badge.'),
+      '#options' => ['icons' => $this->t('Small icon'), 'buttons' => $this->t('Large button with text')],
+      '#default_value' => $this->getSetting('button_style') ?? 'buttons',
+    ];
+
+    $form['image_height'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Image Height'),
+      '#description' => $this->t('The height in pixels for the Rightsstatements badge image.'),
+      '#default_value' => $this->getSetting('image_height') ?? '31',
+    ];
+
+    $form['image_color'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Image Colour'),
+      '#description' => $this->t('The color of the text and image in the Rightsstatements badge.'),
+      '#options' => [
+        'dark' => $this->t('Black with transparent icon interior'),
+        'white' => $this->t('All white'),
+        'dark-white-interior' => $this->t('Black with white icon interior'),
+        'dark-white-interior-blue-type' => $this->t('Black with blue type (Button style only)'),
+      ],
+      '#default_value' => $this->getSetting('image_color') ?? 'dark',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $summary[] = $this->t('Button Style: @field', ['@field' => $this->getSetting('button_style')]);
+    $summary[] = $this->t('Image height: @field', ['@field' => $this->getSetting('image_height')]);
+    $summary[] = $this->t('Image color: @field', ['@field' => $this->getSetting('image_color')]);
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = $this->getBadge($item->value);
+    }
+    return $elements;
+  }
+
+  /**
+   * Take URI, return drupal render element for a rights statement badge.
+   */
+  public function getBadge($uri) : array {
+    $height = $this->getSetting('image_height');
+    $color = $this->getSetting('image_color');
+    $style = $this->getSetting('button_style');
+
+    // Just print the text if this isn't a rights statement URI.
+    if (filter_var($uri, FILTER_VALIDATE_URL) === FALSE || strpos($uri, '/vocab/') === FALSE) {
+      return ['#markup' => $uri];
+    }
+
+    // Extract just the statement terms from the URI to use in image URL.
+    $terms = substr($uri, strpos($uri, "/vocab/") + 7);
+    $terms = substr($terms, 0, strlen($terms) - 5);
+    if ($style == 'icons') {
+      $style_filename = "Icon-Only.";
+      // In case an unavailable color is picked.
+      if ($color == 'dark-white-interior-blue-type') {
+        $color = 'dark-white-interior';
+      }
+      // Choose appropriate icon since they are less precise than buttons.
+      if ($terms == 'CNE' || $terms == 'UND' || $terms == 'NKC') {
+        $terms = 'Other';
+      }
+      elseif (strpos($terms, 'InC') !== FALSE) {
+        $terms = 'InC';
+      }
+      elseif (strpos($terms, 'NoC') !== FALSE) {
+        $terms = 'NoC';
+      }
+    }
+    else {
+      $style_filename = "";
+    }
+    $imagepath = \Drupal::service('extension.list.module')->getPath('islandora_rights_statements');
+    $image = '/' . $imagepath . '/images/' . $style . '/' . $terms . '.' . $style_filename . $color . '.png';
+
+    $options = [
+      'headers' => [
+        'Accept' => 'application/json+ld',
+      ],
+    ];
+
+    $client = \Drupal::httpClient();
+    try {
+      $response = $client->request('GET', $uri, $options);
+    }
+    catch (\Exception $e) {
+      return ['#markup' => $uri];
+    }
+    $result = json_decode($response->getBody()->getContents(), TRUE);
+
+    $location = $result['headers']['Location'];
+    $responseRedirect = $client->request('GET', 'http://rightsstatements.org' . $location, $options);
+    $resultRedirect = json_decode($responseRedirect->getBody()->getContents(), TRUE);
+
+    $definitions = $resultRedirect['definition'];
+
+    $preferredLanguage = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '';
+    $preferredLanguage = substr(strtok(strip_tags($preferredLanguage), ','), 0, 2);
+    $possibleLanguages = ['de', 'et', 'fi', 'fr', 'pl', 'en', 'sv-fi', 'es'];
+    $language = in_array($preferredLanguage, $possibleLanguages) ? $preferredLanguage : 'en';
+
+    $definitionValue = '';
+    foreach ($definitions as $definition) {
+      if ($definition['@language'] == $language) {
+        $definitionValue = $definition['@value'];
+        break;
+      }
+    }
+
+    $notes = $resultRedirect['note'];
+    $noteValue = '';
+    foreach ($notes as $note) {
+      if ($note['@language'] == $language) {
+        $noteValue = $note['@value'];
+        break;
+      }
+    }
+
+    $titles = $resultRedirect['prefLabel'];
+    $titleValue = '';
+    foreach ($titles as $title) {
+      if ($title['@language'] == $language) {
+        $titleValue = $title['@value'];
+        break;
+      }
+    }
+
+    $altText = $titleValue . '<p>' . $definitionValue . '</p><p>' . $noteValue . '</p>';
+    $badge = [
+      '#type' => 'link',
+      '#title' => [
+        '#type' => 'html_tag',
+        '#tag' => 'img',
+        '#attributes' => [
+          'src' => $image,
+          'alt' => $altText,
+          'height' => $height,
+          'title' => $titleValue,
+        ],
+      ],
+      '#url' => Url::fromUri($uri),
+      '#options' => [
+        'attributes' => [
+          'target' => '_blank',
+        ],
+        'html' => TRUE,
+      ],
+    ];
+
+    return $badge;
+  }
+
+}
